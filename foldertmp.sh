7f3f03024842dc4152af1d7c07c7652ab5ca3b21
Jean-Christophe Champarnaud <jcchamparnaud@mesdepanneurs.fr>
2020-10-14 12:40:55 +0200

    deplacement foldertmp.sh

---
+++ foldertmp.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#-------------------------------------------------------
+# Script de vérification de présence du répertoire 'jcc'
+# dans le répertoire temporaire '/tmp'
+#-------------------------------------------------------
+
+quisuisje=$(whoami)
+
+# vérification si /tmp/jcc existe sinon création	
+[ -d /tmp/jcc ] || mkdir /tmp/"$quisuisje"
+
+# vérification si le raccourci est présent dans /home/<user>
+[ -h ~/tmp ] || ln -s /tmp/"$quisuisje" ~/tmp
+
+# ajout du cron
+testcron=$(crontab -l | grep "foldertmp")
+[ ! -z "$testcron" ] || (crontab -l; echo "10 * * * * cd ~/.dotfiles/conf && sh foldertmp.sh") | crontab -
+
+exit 0
