959b90b9c7e900a754a88b75e27c9b846c860e1b
Jean-Christophe Champarnaud <jc@champarnaud.fr>
2021-11-04 21:20:34 +0100

    refactorisation du menu

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@
 export LC_ALL=C
 
 #--- ENVIRONNEMENT
-repconf="conf"
-repscripts="scripts"
-[ ! -s "$repconf/machine" ] || echo "" > "$repconf/machine"
+REPCONF="conf"
+REPSCRIPTS="scripts"
+[ ! -s "$REPCONF/machine" ] || echo "" > "$repconf/machine"
 
 #--- LISTE DES FICHIERS ET REPERTOIRES DE CONFIGURATION
 tab=($(ls conf/ && ls scripts/))
@@ -13,41 +13,39 @@ tab=($(ls conf/ && ls scripts/))
 #--- FONCTIONS
 function creation_de_liens_symboliques() {
 
-	app=$1 # nom de l'application
-
 	# création du lien symbolique vers le fichier dans le rep 'conf/'
-	msg=$(ln -s ~/.dotfiles/$repconf/"$app" ~/."$app" 2>&1)
+	msg=$(ln -s ~/.dotfiles/$REPCONF/"$1" ~/."$1" 2>&1)
 
 	# Si l'ancien fichier ou lien symbolique existe on le supprime
 	# pour le remplacer
 	if [ ! -z "$msg" ]; then
-		read -p "Voulez-vous supprimer le lien '$app' précédent ? [O/n] " suppr
+		read -p "Voulez-vous supprimer le lien '$1' précédent ? [O/n] " suppr
 		if [[ $suppr =~ [oOyY] ]]; then
-			echo "Essai de suppression de : ."$app
-			msg=$(rm ~/."$app" 2>&1)
+			echo "Essai de suppression de : ."$1
+			msg=$(rm ~/."$1" 2>&1)
 			if [ -z $msg ]; then
-				echo "Suppression de ===> ."$app
+				echo "Suppression de ===> ."$1
 				creation_de_liens_symboliques $1
 			else
-				echo "Impossible de supprimer : ."$app
+				echo "Impossible de supprimer : ."$1
 				echo "Vérifiez que vous avez les droits"
 			fi
 		else
-			echo "Vous devez supprimer le fichier ."$app
+			echo "Vous devez supprimer le fichier ."$1
 		fi
 	else
-		echo "Installation de ===> ."$app
+		echo "Installation de ===> ."$1
 		# si installation de la machine
-		if [[ ! -s "$repconf/machine" || "$app" == "machine" ]]; then
+		if [[ ! -s "$REPCONF/machine" || "$1" == "machine" ]]; then
 			read -p "Donnez un nom à votre machine : " machine
-			echo "$machine" > "$repconf/machine"
+			echo "$machine" > "$REPCONF/machine"
 		fi
 	fi
 }
 
 function execution_de_script(){
 	echo "execusion de script : $1 " 
-	sh "$repscripts/$1}"
+	sh "$REPSCRIPTS/$1}"
 }
 
 #--- main
